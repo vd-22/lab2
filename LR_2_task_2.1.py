c56e43f704a78c044ec33f6e3e2ac1afef6cfe55
dariav <vitiukd22@gmail.com>
2026-05-09 18:41:59 +0200

    init commit lr2_2.1

---
+++ LR_2_task_2.1.py
@@ -0,0 +1,32 @@
+import numpy as np
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# Вхідний файл
+input_file = 'income_data.txt'
+
+# Читання даних
+X = []
+count_class1 = 0
+count_class2 = 0
+max_datapoints = 25000
+
+with open(input_file, 'r', encoding='utf-8') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+
+        if '?' in line:
+            continue
+
+        data = line.strip().split(', ')
+
+        if data[-1] == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+        elif data[-1] == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
